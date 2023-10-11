4c651165ce30a540c873529c72e977f8e932576d
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-11 21:57:25 +0100

    Processamento na página inicail

--- app/views/_inc/footer.php
+++ app/views/_inc/footer.php
@@ -1,18 +1,71 @@
 <footer>
-	<dl>
 
-		<dt>
-			<h3> Sobre </h3>
-		</dt>
+	<div class="container d-table align-items-center">
+            <div class="container d-block d-md-flex pb-2">
+                    <!--SOBRE A EMPRESA-->
+                <div class="col pt-2" style="height: auto;">
+                    <p class="h6 text-left text-md-center pt-2"><b>Sobre</b>
+                        <hr>
+                    </p>
+                    <i class="fas fa-building" style="font-size:24px; margin-right: 20px;"></i>
+                    <div class="col text-left text-justify">
+						Um site fitício, simulação de um sistema de gestão de notas. Este site foi criador pelo estudante Eugénio Cachiombo<br>
+                    </div>
+                    <hr>
+                </div>
 
-		<dd>
-			Este site foi criador pelo estudante Eugénio Cachiombo do curso de Engenharia Informática. <br>
-			<strong>Localização:</strong> Município do Cazenga, bairro Mabor / Hoji-yá-Henda<br>
-			<strong>Contactos:</strong> 921439849 <strong>whatsapp:</strong> 921439849 <strong>Facebook:</strong> Génio
-			Pró Gp
+                <!--CONTACTOS-->
+                <div class="col pt-2" style="height: auto;">
+                    <p class="h6 text-left text-md-center pt-2"><b>Contactos</b>
+                        <hr>
+                    </p>
 
-		</dd>
+                    <!--TELEFONE-->
+                    <div class="col d-flex justify-content-left align-items-center">
+                        <i class="fas fa-phone" style="font-size:24px; margin-right: 20px"></i> <b> (+244) 921439849
+                        </b>
+                    </div> <br>
 
-	</dl>
+                    <!--EMAIL EMPRESA-->
+                    <div class="col d-flex justify-content-left align-items-center">
+                        <i class="fas fa-envelope" style="font-size:24px; margin-right: 20px"></i> <b>escolacachiombo@gmail.com
+                        </b>
+                    </div> <br>
+
+                    <!--Whatsapp-->
+                    <div class="col d-flex justify-content-left align-items-center">
+                        <i class="fab fa-whatsapp" style="font-size:24px; margin-right: 20px"></i> <b>(+244) 921439849 </b>
+                    </div> <br>
+
+                    <!--Facebook-->
+                    <div class="col d-flex justify-content-left align-items-center">
+                        <i class="fab fa-facebook" style="font-size:24px; margin-right: 20px"></i><b>Escola Cachiombo Oficial</b>
+
+                    </div> <br>
+                    <hr>
+                </div>
+
+                <!--LOCALIZAÇÃO-->
+                <div class="col pt-2" style="height: auto;">
+                    <p class="h6 text-left text-md-center pt-2"><b>Localização</b>
+                        <hr>
+                    </p>
+                    <i class="fas fa-map-marker-alt" style="font-size:24px; margin-right: 20px;"></i>
+                    <div class="col text-left text-justify">
+                        Província de Luanda <br>
+                        Município do Cazenga, bairro Hoji-Yá-Henda
+                    </div>
+                    <hr>
+
+                </div>
+            </div>
+
+            <!--DIREITOS-->
+            <div class="container d-flex justify-content-center text-center pt-2" >
+                © Editora EC 2023. Todos os direitos reservados. Escola Cachiombo
+            </div> 
+            <br class="d-blcok d-sm-none">
+            <hr class="d-none d-sm-block">
+        </div>
 
 </footer>
