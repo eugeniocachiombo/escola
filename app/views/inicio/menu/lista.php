2eb0624ffe39c897a13a5b521598b8d594751eeb
eugeniocachiombo <eugeniocachiombo@gmail.com>
2023-10-12 03:35:05 +0100

    Problema da pauta quase resolvido

--- app/views/inicio/menu/lista.php
+++ app/views/inicio/menu/lista.php
@@ -1,4 +1,4 @@
 
-<a href="" > <li class="d-flex align-items-center"> <i class="fas fa-book pe-2"></i> Pauta   </li> </a>
+<a href="../pauta" > <li class="d-flex align-items-center"> <i class="fas fa-book pe-2"></i> Pauta   </li> </a>
 <a href="" > <li class="d-flex align-items-center"> <i class="fas fa-user pe-2"></i> Sessao  </li> </a>
 <a href="" > <li class="d-flex align-items-center"> <i class="fas fa-info-circle pe-2"></i> Info    </li> </a>
---
+++ app/views/pauta/index.php
@@ -0,0 +1,217 @@
+<?php include '../inicio/menu/bars.php';?>
+<?php include '../_inc/headHTML.php';?>
+<?php include '../_inc/header.php';?>
+<?php include '../../config/db/connection.php';?>
+<title>Pauta</title>
+
+<head>
+	<title>Login</title>
+	<meta charset="utf-8">
+	<style type="text/css">
+		table {
+			width: 100%;
+			text-align: center;
+		}
+
+		th {
+			min-width: 150px;
+			border: 1px solid;
+		}
+
+		#pad {
+			padding: 1px;
+			display: block;
+		}
+
+		#p {
+			text-align: right;
+		}
+
+		th {
+			background: #04b131;
+		}
+
+		#a {
+			background: gray;
+		}
+
+		#positiva {
+			background: #0054fe;
+		}
+
+		#negativa {
+			background: #fe0000;
+		}
+
+		#posinota {
+			background: #04b131;
+			color: white;
+		}
+
+		#neganota {
+			background: #717171;
+			color: #f85151;
+		}
+
+		#c {
+			background: gray;
+		}
+
+		#a1 {
+			background: #04b131;
+			color: white;
+		}
+
+		#a2 {
+			background: #06912a;
+			color: white;
+		}
+
+		#a3 {
+			background: #047922;
+			color: white;
+		}
+
+
+		#a4 {
+			background: #004c14;
+			color: white;
+		}
+	</style>
+</head>
+
+<body>
+
+	<center>
+		<a href="Vermelhor.php" style="background: green; color: white;">Ver Melhor Classificação</a>
+	</center> <br>
+
+	<center>
+		<a href="Reprovados.php" style="background: green; color: white;">Reprovados </a>
+	</center> <br>
+
+	<center>
+		<a href="Página Inicial.php" style="background: green; color: white;">
+			Página Inicial
+		</a>
+	</center><br>
+
+	<div class="table-responsive">
+		<table border="1" align="center">
+			<tr id="cab">
+				<th> Nome do Aluno </th>
+				
+				<?php
+
+					$con = GetConnection();
+					$sql_disc = "select distinct nome_disc from pauta 
+					left outer join aluno
+					on pauta.id_aluno = aluno.id_aluno
+					left outer join disciplina
+					on pauta.id_disc = disciplina.id_disc ";
+					$stmt_disc = $con->prepare($sql_disc);
+					$stmt_disc->execute();
+					$result_disc = $stmt_disc->fetchAll();
+					$nome_disc_save = array();
+					$end_cont = 0;
+					
+					foreach ($result_disc as $value) {
+						?> 
+							<th> 
+								<?php  echo $value["nome_disc"]; ?> 
+							</th> 
+						<?php
+						array_push($nome_disc_save, $value["nome_disc"]);
+						$end_cont++;
+					}
+				?>
+			</tr>
+
+			<?php
+				$sql_disc = "select * from pauta 
+				left outer join aluno
+				on pauta.id_aluno = aluno.id_aluno
+				left outer join disciplina
+				on pauta.id_disc = disciplina.id_disc order by nome_aluno asc";
+				$stmt_disc = $con->prepare($sql_disc);
+				$stmt_disc->execute();
+				$result_disc = $stmt_disc->fetchAll();
+				$cont = 0;
+				
+				foreach ($result_disc as $value) {
+					?> 
+						<tr> 
+							<?php if($cont == 0){ ?>
+								<td> <?php  echo $value["id_aluno"] . ": " . $value["nome_aluno"]; ?> </td>
+								 
+								 <?php
+									$sql_disc = "select nota from pauta where id_aluno = ?";
+									$stmt_disc = $con->prepare($sql_disc);
+									$stmt_disc->bindValue(1, $value["id_aluno"]);
+									$stmt_disc->execute();
+									$result_disc = $stmt_disc->fetchAll();
+								 ?>
+									<?php foreach ($result_disc as $value) { ?>
+										<td > <?php  echo $value["nota"]; ?> </td> 
+									<?php } ?>
+							<?php } ?>
+						</tr> 
+					<?php
+					$cont++;
+					if($cont == $end_cont){$cont=0;}
+				}
+				
+			?>
+			
+			<?php	
+			/*
+			foreach ($result as $value) { ?>
+
+			<tr>
+				<?php $cont = $cont + 1;     
+
+
+			if ($cont == 1) {
+			?>
+				<td id="a1"> <?php echo $value["disciplina"]?> </td>
+
+				<?php	} else if ($cont == 2) {?>
+				<td id="a2"> <?php echo $value["disciplina"]?> </td>
+
+				<?php	} elseif ($cont == 3) {?>
+				<td id="a3"> <?php echo $value["disciplina"]?> </td>
+
+				<?php	} elseif ($cont == 4) {?>
+				<td id="a4"> <?php echo $value["disciplina"]?> </td>
+
+				<?php	$cont = $cont - 4; 
+			} 
+			?>
+
+				<?php 
+
+			if ($value["nota"] >= 9.5) { ?>
+
+				<td id="positiva"> <?php echo $value["nomeAluno"]?> </td>
+				<td id="posinota"> <?php echo intval($value["nota"]) ?> </td>
+
+				<?php	} else { ?>
+
+				<td id="negativa"> <?php echo $value["nomeAluno"]?> </td>
+				<td id="neganota"> <?php echo intval($value["nota"])?> </td>
+
+				<?php	}?>
+
+			</tr>
+
+			<?php	
+
+			//<h3>Em breve serão lançadas as pautas dos alunos neste site</h3>
+
+
+			} */?>
+		</table>
+	</div>
+</body>
+
+</html>
